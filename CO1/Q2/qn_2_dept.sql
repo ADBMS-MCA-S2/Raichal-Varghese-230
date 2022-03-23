350b90da64135027ae1098d6091901cc670d3bb9
Raichal <raichalvarghese231@gmail.com>
2022-03-23 17:39:34 +0530

    co1

---
+++ CO1/Q2/qn_2_dept.sql
@@ -0,0 +1,14 @@
+create database DEPARTMENT;
+use DEPARTMENT;
+drop table Dept;
+CREATE TABLE Dept(
+DEPTNO int,
+DNAME VARCHAR(20),
+LOC varchar(15)
+);
+alter table Dept rename department;
+alter table department add PINCODE VARCHAR(6) NOT NULL;
+alter table department drop column PINCODE;
+ALTER TABLE department rename column DNAME to DEPT_NAME;
+ALTER TABLE department modify LOC varchar(10);
+drop table department;
