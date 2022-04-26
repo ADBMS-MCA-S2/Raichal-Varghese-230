bc54d08ced9a5db799db2bcb6f0a498c0a952ac5
raichalvarghese <53505130+raichalvarghese@users.noreply.github.com>
2022-04-27 04:22:48 +0530

    27/04/22

---
+++ CO2/Trigger/group_assign_trigger/shop.sql
@@ -0,0 +1,7 @@
+create database shop;
+use shop;
+create table product(p_id int,p_name varchar(20),quantitiy int,price int);
+create table product(c_id int,c_name varchar(20),p_id int ,purchased_quant int);
+create table product(c_id int,c_name varchar(20),p_id int ,purchased_quant int);
+create table product(c_id int,c_name varchar(20),p_id int ,purchased_quant int);
+create table product(c_id int,c_name varchar(20),p_id int ,purchased_quant int);
